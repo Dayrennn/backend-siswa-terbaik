0b96bd150925ad108599c959c13f6b08f17bde59
Dayrennn <raflyramandha789@gmail.com>
2026-06-27 02:22:08 +0700

    [ FIX ] - balikin lagi ke none

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -52,7 +52,7 @@ export const login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: false,
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
