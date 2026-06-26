f14839f3b056ffbebe35b38291ab77f40c3f3242
Dayrennn <raflyramandha789@gmail.com>
2026-06-27 01:56:57 +0700

    [ FIX ] - cors

--- src/server/server.js
+++ src/server/server.js
@@ -23,7 +23,7 @@ const port = 3000;
 
 app.use(morgan('dev'));
 
-app.use(cors({ origin: 'http://localhost:3001', credentials: true }));
+app.use(cors({ origin: 'https://frontend-skripsi.netlify.app/', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
